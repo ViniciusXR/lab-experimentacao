b321f5157cb0a7231f70f2c4e4da5ad98372d691
Vinicius Xavier <viniciusxr89@gmail.com>
2026-02-25 18:35:53 -0300

    adicionando readme e spectre console

--- Enunciado 1/Sprint 2/Program.cs
+++ Enunciado 1/Sprint 2/Program.cs
@@ -5,6 +5,7 @@ namespace Enunciado1.Sprint2
         static void Main(string[] args)
         {
             Console.WriteLine("Enunciado 1 - Sprint 2");
+            Console.ReadKey();
         }
     }
 }
